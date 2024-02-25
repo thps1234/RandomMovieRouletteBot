e23c7a360f903819c211805e73117173973da57c
thps1234 <thps1234@gmail.com>
2024-02-25 16:05:41 +0300

    1

--- randomMovieRouletteBot.py
+++ randomMovieRouletteBot.py
@@ -3,11 +3,11 @@ from telebot import types
 
 bot = telebot.TeleBot('6470319699:AAFZAv2AulBCohVSY4HVkKschZcJwudnjPs')
 
-@bot.message_handler(commands=['start'])
+bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Roll")
-    markup.add(btn1)
-    bot.send_message(message.from_user.id, "Roll", reply_markup=markup)
+    btn = types.KeyboardButton("Roll")
+    markup.add(btn)
+    bot.send_message(message.from_user.id, "Test", reply_markup=markup)
 
 bot.polling(none_stop=True, interval=0)
