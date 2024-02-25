3a56624621c58733024e34ae02a6daf0ac94b9c4
thps1234 <thps1234@gmail.com>
2024-02-25 15:58:37 +0300

    Initial commit

---
+++ randomMovieRouletteBot.py
@@ -0,0 +1,12 @@
+import telebot
+
+bot = telebot.TeleBot('6470319699:AAFZAv2AulBCohVSY4HVkKschZcJwudnjPs')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Roll")
+    markup.add(btn1)
+    bot.send_message(message.from_user.id, "Roll", reply_markup=markup)
+
+bot.polling(none_stop=True, interval=0)
