0bd008c8a573e8e85e2263f511d5d8bd5b1cb4be
thps1234 <thps1234@gmail.com>
2024-03-02 11:32:51 +0300

    1

--- randomMovieRouletteBot.py
+++ randomMovieRouletteBot.py
@@ -1,9 +1,20 @@
-from aiogram import Bot, Dispatcher, executor, types
-
-API_TOKEN = '6470319699:AAFZAv2AulBCohVSY4HVkKschZcJwudnjPs'
-bot = Bot(token = API_TOKEN)
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+TOKEN = "6470319699:AAFZAv2AulBCohVSY4HVkKschZcJwudnjPs"
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start']) #Явно указываем в декораторе, на какую команду реагируем.
-async def send_welcome(message: types.Message):
-    await message.reply("Привет!\nЯ Эхо-бот от Skillbox!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(msg: types.Message):
+    await msg.reply_to_message(f'Я бот. Приятно познакомиться, {msg.from_user.first_name}')
+
+@dp.message_handler(content_types=['text'])
+async def get_text_messages(msg: types.Message):
+    if msg.text.lower() == 'привет':
+        await msg.answer('Привет!')
+    else:
+        await msg.answer('Не понимаю, что это значит.')
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
