1d1a49c97d9cb68eefa56400695d8a80246732ce
thps1234 <thps1234@gmail.com>
2024-04-29 10:45:47 +0300

    beta

--- randomMovieRouletteBot.py
+++ randomMovieRouletteBot.py
@@ -1,20 +1,93 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
-TOKEN = "6470319699:AAFZAv2AulBCohVSY4HVkKschZcJwudnjPs"
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+import telebot
+from telebot import types
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(msg: types.Message):
-    await msg.reply_to_message(f'Я бот. Приятно познакомиться, {msg.from_user.first_name}')
+import json
 
-@dp.message_handler(content_types=['text'])
-async def get_text_messages(msg: types.Message):
-    if msg.text.lower() == 'привет':
-        await msg.answer('Привет!')
+import requests
+from bs4 import BeautifulSoup
+import random
+
+bot = telebot.TeleBot(open("token.txt", "r").read())
+
+headers = json.loads(open("headers.json", "r").read())
+
+lt_lists = json.loads(open("lists.json", "r").read())
+
+keyboard = types.InlineKeyboardMarkup()
+for i in lt_lists.keys():
+    key = types.InlineKeyboardButton(text=i, callback_data=i)
+    keyboard.add(key)
+
+
+def get_urls_from_list(i_url):
+    lt_urls = []
+    lv_url = i_url
+    lv_page = 1
+    page = requests.get(lv_url, headers=headers)
+    while 1 == 1:
+        lv_prev = len(lt_urls)
+        lv_page += 1
+        soup = BeautifulSoup(page.content, "html.parser")
+        for element in soup.find_all('div', class_="really-lazy-load"):
+            lt_urls.append(element.attrs['data-target-link'])
+        if lv_prev == len(lt_urls):
+            return lt_urls
+        lv_url = i_url + '/page/' + str(lv_page)
+        page = requests.get(lv_url, headers=headers)
+
+
+def get_movie_attrs(i_url):
+    lt_attr = dict()
+    page = requests.get(i_url, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+    element = soup.find("section", class_="film-header-lockup")
+    if len(element.contents[3].contents) == 5:
+        lt_attr["Name"] = element.contents[1].text
+        lt_attr["Directed by"] = element.contents[3].contents[3].text
+        lt_attr["Year"] = element.contents[3].contents[1].text
     else:
-        await msg.answer('Не понимаю, что это значит.')
+        lt_attr["Name"] = element.contents[1].text
+        lt_attr["Original Name"] = element.contents[3].contents[3].text
+        lt_attr["Directed by"] = element.contents[3].contents[5].text
+        lt_attr["Year"] = element.contents[3].contents[1].text
+    return lt_attr
 
-if __name__ == '__main__':
-    executor.start_polling(dp)
+
+def get_random_url_from_list(i_url):
+    page = requests.get(i_url, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+    lv_description = soup.find_all("meta")[3].attrs["content"]
+    lv_number = int(lv_description.split(" ", 4)[3].replace(",", ""))
+    lv_index = random.randrange(lv_number)
+    lv_page = lv_index // 100 + 1
+    lv_num_on_page = lv_index % 100 + 1
+    lv_url = i_url + "/page/" + str(lv_page)
+    page = requests.get(lv_url, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+    return "https://letterboxd.com/" + \
+        soup.find_all("div", class_="really-lazy-load")[lv_num_on_page].attrs["data-target-link"]
+
+
+def get_message(i_movie_url):
+    lv_message = "<b><a href=\"" + i_movie_url + "\">" + get_movie_attrs(i_movie_url)["Name"] + "</a></b>" + "\n" + \
+                 "Directed by " + get_movie_attrs(i_movie_url)["Directed by"] + "\n" \
+                 "Year: " + get_movie_attrs(i_movie_url)['Year']
+    return lv_message
+
+
+@bot.message_handler(content_types=["text"])
+def get_text_messages(message):
+    if message.text == "/start":
+        bot.send_message(message.from_user.id, text="<b>Choose from list or send URL:</b>",
+                         reply_markup=keyboard, parse_mode="HTML")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    bot.send_message(call.message.chat.id,
+                     get_message(get_random_url_from_list(lt_lists[call.data])), parse_mode="HTML")
+    bot.send_message(call.message.chat.id, text="<b>Choose from list or send URL:</b>",
+                     reply_markup=keyboard, parse_mode="HTML")
+
+
+bot.polling(none_stop=True, interval=0)
