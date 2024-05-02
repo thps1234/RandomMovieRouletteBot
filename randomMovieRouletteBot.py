ded20a31d1753300afdc629f438902ac0dd9aa6b
thps1234 <thps1234@gmail.com>
2024-05-02 15:53:20 +0300

    1.04
    Bugfixes

--- randomMovieRouletteBot.py
+++ randomMovieRouletteBot.py
@@ -48,13 +48,14 @@ def get_movie_attrs(i_url):
 #picks a random movie from a list
 def get_random_url_from_list(i_url):
     page = requests.get(i_url, headers=headers)
+    i_url = page.url #making a big URL out of short
     soup = BeautifulSoup(page.content, "html.parser")
     lv_description = soup.find_all("meta")[3].attrs["content"]
     lv_number = int(lv_description.split(" ", 4)[3].replace(",", ""))
     lv_index = random.randrange(lv_number)
     lv_page = lv_index // 100 + 1
-    lv_num_on_page = lv_index % 100 + 1
-    lv_url = i_url + "/page/" + str(lv_page)
+    lv_num_on_page = lv_index % 100
+    lv_url = i_url + "page/" + str(lv_page)
     page = requests.get(lv_url, headers=headers)
     soup = BeautifulSoup(page.content, "html.parser")
     return "https://letterboxd.com" + \
